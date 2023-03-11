74b75cd92e3958ee55a6d37004bec73e5cbb53d5
Auroiah Morgan <amwebdev@outlook.com>
2023-03-11 15:47:19 -0500

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,14 @@
 import { Container } from "@mui/material";
 import SignupForm from "../components/SignupForm";
-import { redirect } from "react-router-dom";
-export default function Signup() {
+import { useLocation, useNavigate } from "react-router-dom";
 
+
+export default function Signup() {
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
     const handleSuccessSignup = () => {
-        return redirect('/');
+        return navigate(from, { replace: true });
     }
     return (
         <Container maxWidth="xs"
